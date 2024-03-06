7b070e3df3ba8b9761d4bc9719a5fea31af12e10
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2024-03-06 14:59:34 -0800

    dog

---
+++ 0x18-webstack_monitoring/checks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Get all dashboard lists returns 'ok'  response
+'''
+
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.dashboard_lists_api import DashboardListsApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = DashboardListsApi(api_client)
+    response = api_instance.list_dashboard_lists()
+
+    print(response)
